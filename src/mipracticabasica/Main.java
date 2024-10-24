380e37e5b0510a91963e86918a32e42863fe83fb
Ivan-Castill <ivan2003castillo12345@gmail.comzx>
2024-10-24 09:10:55 -0500

    Taller en Clases

--- src/mipracticabasica/Main.java
+++ src/mipracticabasica/Main.java
@@ -1,5 +1,11 @@
+package mipracticabasica;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Ingrese su edad:");
+        int edad=scanner.nextInt();
+        Persona person1=new Persona(edad);
+        person1.mostrarResultado();
     }
 }
--- src/mipracticabasica/Persona.java
+++ src/mipracticabasica/Persona.java
@@ -1,4 +1,27 @@
 package mipracticabasica;
 
 public class Persona {
+    int edad;
+
+    public Persona(int edad){
+        this.edad=edad;
+    }
+    public void imprimirEdad(){
+        System.out.println("La edad es : "+ edad);
+    }
+    public boolean verificarMayorDeEdad() {
+        if (edad >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void mostrarResultado(){
+        System.out.println("Tu edad es: "+edad);
+        if (verificarMayorDeEdad()) {
+            System.out.println("El usuario es mayor de edad.");
+        } else {
+            System.out.println("El usuario es menor de edad.");
+        }
+    }
 }
